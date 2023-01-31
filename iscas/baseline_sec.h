c6b4419ec4852f433250e6ce4b5841caaf3deb24
Rafael Faccenda <faccendarafael@gmail.com>
2023-01-31 16:03:11 -0300

    [ISCAS23] Testcase sync

---
+++ iscas/baseline_sec.h
@@ -0,0 +1,18 @@
+/* hardware/sc/peripherals/IO_peripheral/sabotage_param.h */
+
+#define T_START         40000000  //100000
+#define T_END           75000000  //120000
+#define PERIOD          3       //500
+
+#define HEADER_FIX_HI   0x0000  //header  Source: 0203 (usando 12 bits)
+#define HEADER_FIX_LO   0x0000  //header 
+
+#define HEADER_ROUT_HI  0x0000  //header  Source: 0203 (usando 12 bits)
+#define HEADER_ROUT_LO  0x0000  //header
+
+#define F1_FLIT         0x0000  //f1
+#define F2_FLIT         0x0000  //f2
+
+#define SERVICE_FLIT    0x0000  //service
+
+#define SOURCE_FLIT     0x0000
