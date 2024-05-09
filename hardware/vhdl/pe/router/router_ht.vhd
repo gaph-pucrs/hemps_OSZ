8c2510b4624db12880d07814cf70885058001aba
Gustavo Comarú <gustavo.comaru.rod@gmail.com>
2024-05-09 14:42:43 -0300

    Intermittent DoS HT - Masks credit down to delay packets

--- hardware/vhdl/pe/router/router_ht.vhd
+++ hardware/vhdl/pe/router/router_ht.vhd
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
 use work.standards.all; 
 use work.hemps_pkg.all; 
 
@@ -96,4 +97,118 @@ begin
 
     activated <= '1' when counter=trigger_time else '0';
 
+end architecture;
+
+architecture router_ht_intermittent of router_ht is
+
+    constant COUNTER_LENGTH     : integer := 16;
+    constant LFSR_FIXED_TURNS   : integer := 8;
+    constant LFSR_POLYNOMIAL    : std_logic_vector(COUNTER_LENGTH-1 downto 0) := x"D008";
+    constant LFSR_SEED          : std_logic_vector(COUNTER_LENGTH-1 downto 0) := x"ABBA";
+
+    type StateType is (GEN_DISABLED_TIME, DISABLED, GEN_ENABLED_TIME, ENABLED);
+    signal state        : StateType;
+    signal next_state   : StateType;
+
+    signal counter      : std_logic_vector(COUNTER_LENGTH-1 downto 0);
+    signal lfsr         : std_logic_vector(COUNTER_LENGTH-1 downto 0);
+    signal lfsr_mask    : std_logic_vector(COUNTER_LENGTH-1 downto 0);
+
+    signal credit_mask : std_logic;
+
+begin
+
+    ht_tx           <= router_tx;
+    ht_clock_tx     <= router_clock_tx;
+    ht_data_out     <= router_data_out;
+    ht_eop_out      <= router_eop_out;
+    router_cred_in  <= ht_cred_in and credit_mask;
+
+    credit_mask <= '0' when (state=ENABLED or state=GEN_DISABLED_TIME) else '1';
+
+    ChangeState: process(clock, reset)
+    begin
+        if reset='1' then
+            state <= GEN_DISABLED_TIME;
+        elsif rising_edge(clock) then
+            state <= next_state;
+        end if;
+    end process;
+
+    NextStateLogic: process(state)
+    begin
+        case state is
+
+            when GEN_DISABLED_TIME =>
+
+                if counter=0 then
+                    next_state <= DISABLED;
+                else
+                    next_state <= GEN_DISABLED_TIME;
+                end if;
+            
+            when DISABLED =>
+
+                if counter=0 then
+                    next_state <= GEN_ENABLED_TIME;
+                else
+                    next_state <= DISABLED;
+                end if;
+            
+            when GEN_ENABLED_TIME =>
+
+                if counter=0 then
+                    next_state <= ENABLED;
+                else
+                    next_state <= GEN_ENABLED_TIME;
+                end if;
+
+            when ENABLED =>
+
+                if counter=0 then
+                    next_state <= GEN_DISABLED_TIME;
+                else
+                    next_state <= ENABLED;
+                end if;
+        
+        end case;
+    end process;
+
+    Countdown: process(clock, reset)
+    begin
+        if reset='1' then
+            counter <= conv_std_logic_vector(LFSR_FIXED_TURNS, counter'length);
+        elsif rising_edge(clock) then 
+
+            -- set counter for next state
+            if counter=0 then
+                if state=GEN_DISABLED_TIME or state=GEN_ENABLED_TIME then
+                    counter <= lfsr;
+                elsif state=DISABLED or state=ENABLED then
+                    counter <= conv_std_logic_vector(LFSR_FIXED_TURNS, counter'length);
+                end if;
+            
+            -- decrement counter
+            else
+                counter <= counter - 1;
+            end if;
+
+        end if;
+    end process;
+
+    GenMaskLFSR: for k in COUNTER_LENGTH-1 downto 0 generate
+        lfsr_mask(k) <= lfsr_polynomial(k) and lfsr(0);
+    end generate;
+
+    ShiftLFSR: process(clock, reset)
+    begin
+        if reset ='1' then
+            lfsr <= LFSR_SEED;
+        elsif rising_edge(clock) then
+            if state=GEN_DISABLED_TIME or state=GEN_ENABLED_TIME then
+                lfsr <= ('0' & lfsr(COUNTER_LENGTH-1 downto 1)) xor lfsr_mask;
+            end if;
+        end if;
+    end process;
+            
 end architecture;
