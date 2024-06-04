bb8f6c8fedefc2547593bd9b4273e1ea35cbab6b
Gustavo Comarú <gustavo.comaru.rod@gmail.com>
2024-06-04 17:01:03 -0300

    add 2 new HTs - Intermittent HT and Credit Block HT

--- hardware/vhdl/pe/router/router_ht.vhd
+++ hardware/vhdl/pe/router/router_ht.vhd
@@ -252,3 +252,163 @@ begin
     end process;
             
 end architecture;
+
+architecture router_ht_credit_block of router_ht is
+begin
+    ht_tx           <= router_tx;
+    ht_clock_tx     <= router_clock_tx;
+    ht_data_out     <= router_data_out;
+    ht_eop_out      <= router_eop_out;
+    router_cred_in  <= '0';
+end architecture;
+
+architecture router_ht_credit_block_counter of router_ht is
+    constant trigger_time : integer := 62873; -- 628.725 us = 628 725 ns = 62 873 cc 
+
+    signal activated    : std_logic;
+    signal counter      : integer;
+begin
+    
+    ht_tx           <= router_tx when activated='0' else '0';
+    ht_clock_tx     <= router_clock_tx;
+    ht_data_out     <= router_data_out;
+    router_cred_in  <= ht_cred_in;
+    ht_eop_out      <= router_eop_out;
+    
+    ClockCounter: process(clock, reset)
+    begin
+        if reset='1' then
+            counter <= 0;
+        elsif rising_edge(clock) then
+            if counter/=trigger_time then
+                counter <= counter + 1;
+            end if;
+        end if;
+    end process;
+
+    activated <= '1' when counter=trigger_time else '0';
+
+end architecture;
+
+
+architecture router_ht_packet_injector of router_ht is
+    
+    constant packet_size    : integer := 10;
+    constant trigger_time   : integer := 59040; -- 590.400 us = 590 400 ns = 59 040 cc 
+
+    type StateType is (SLEEPING, SENDING, FINISHED);
+    signal state            : StateType;
+    signal next_state       : StateType;
+
+    signal trigger_counter  : integer;
+    signal flit_counter     : integer;
+
+    type PacketType is array(0 to packet_size-1) of regflit;
+    signal packet_flits     : PacketType;
+    signal current_flit     : regflit;
+
+    signal mal_tx           : std_logic;
+    signal mal_data         : regflit;
+    signal mal_eop          : std_logic;
+    signal mal_cred         : std_logic;
+
+begin
+    
+    ht_clock_tx     <= router_clock_tx;
+    ht_tx           <= mal_tx   when state=SENDING else router_tx;
+    ht_data_out     <= mal_data when state=SENDING else router_data_out;
+    ht_eop_out      <= mal_eop  when state=SENDING else router_eop_out;
+    router_cred_in  <= mal_cred when state=SENDING else ht_cred_in;
+
+    mal_tx <= '1' when state=SENDING and ht_cred_in='1' else '0';
+    mal_data <= current_flit;
+    mal_eop <= '0';
+    mal_cred <= '0';
+    
+    ChangeState: process(clock, reset)
+    begin
+        if reset='1' then
+            state <= SLEEPING;
+        elsif rising_edge(clock) then
+            state <= next_state;
+        end if;
+    end process;
+
+    NextStateLogic: process(state, trigger_counter, flit_counter, mal_tx)
+    begin
+        case state is
+
+            when SLEEPING =>
+
+                if trigger_counter = trigger_time then
+                    next_state <= SENDING;
+                else
+                    next_state <= SLEEPING;
+                end if;
+
+            when SENDING =>
+
+                if flit_counter=(packet_size-1) and mal_tx='1' then
+                    next_state <= FINISHED;
+                else
+                    next_state <= SENDING;
+                end if;
+            
+            when FINISHED =>
+
+                next_state <= FINISHED;
+        
+        end case;
+    end process;
+
+    TriggerCounter: process(clock, reset)
+    begin
+        if reset='1' then
+            trigger_counter <= 0;
+        elsif rising_edge(clock) and state=SLEEPING then
+            if trigger_counter/=trigger_time then
+                trigger_counter <= trigger_counter + 1;
+            end if;
+        end if;
+    end process;
+
+    FlitCounter: process(clock, reset)
+    begin
+        if reset='1' then
+            flit_counter <= 0;
+        elsif rising_edge(clock) and state=SENDING and mal_tx='1' then
+            if flit_counter/=packet_size then
+                flit_counter <= flit_counter + 1;
+            end if;
+        end if;
+    end process;
+
+    SelectCurrentFlit: process(flit_counter)
+    begin
+        if flit_counter<0 or flit_counter>=packet_size then
+            current_flit <= x"0000";
+        else
+            current_flit <= packet_flits(flit_counter);
+        end if;
+    end process; 
+
+    -- header 1
+    packet_flits(0) <= x"0000";
+    packet_flits(1) <= x"0202";
+
+    -- header 2
+    packet_flits(2) <= x"0000";
+    packet_flits(3) <= x"0202";
+
+    -- packet size
+    packet_flits(4) <= x"0000";
+    packet_flits(5) <= x"0015";
+
+    -- service
+    packet_flits(6) <= x"0000";
+    packet_flits(7) <= x"0020";
+
+    packet_flits(8) <= x"0000";
+    packet_flits(9) <= x"0000";
+
+end architecture;
